9ec706d65cc9a00ec4a4b3a83ef0459204acfd93
rick427 <44168932+rick427@users.noreply.github.com>
2019-05-11 18:57:55 +0100

    intermediate commit

---
+++ src/Die.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Die.css';
+
+class Die extends Component {
+    render(){
+        return (
+            <div className="Die">
+                <i className={`fas fa-dice-${this.props.face}`}/>
+            </div>
+        )
+    }
+}
+
+export default Die;
